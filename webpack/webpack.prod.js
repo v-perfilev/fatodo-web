04662d42dd9f949860941563c08b11521f0fba40
Vladimir Perfilev <persoff68@gmail.com>
2020-05-05 19:56:59 +0200

    TDM-43 edit webpack

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,51 +1,58 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const commonConfig = require('./webpack.config.js');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const commonConfig = require("./webpack.config.js");
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+const TerserPlugin = require("terser-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-const ENV = 'production';
+const ENV = "production";
 
-module.exports = options => merge(commonConfig({env: ENV}), {
+module.exports = () => merge(commonConfig({ env: ENV }), {
   mode: ENV,
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        enforce: 'pre',
-        loader: 'eslint-loader',
+        enforce: "pre",
+        loader: "eslint-loader",
         exclude: /node_modules/
       },
       {
-        enforce: 'pre',
+        enforce: "pre",
         test: /\.css$/,
-        loader: 'stripcomment-loader'
+        loader: "stripcomment-loader"
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
   optimization: {
     runtimeChunk: false,
-    minimize: true,
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
+          name: "vendors",
+          chunks: "all"
         }
       }
-    }
+    },
+    minimize: true,
+
+    minimizer: [
+      new TerserPlugin()
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        SERVER_API_URL: JSON.stringify('http://localhost:4000/api')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        SERVER_API_URL: JSON.stringify("http://localhost:4000/api")
       }
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin()
   ]
-})
-;
+});
