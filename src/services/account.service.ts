47ab6077d01e8f12b3935393bb5671c97a55b1d3
Vladimir Perfilev <persoff68@gmail.com>
2020-06-02 09:51:13 +0200

    TDM-47 edit account service

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {LoginDTO} from '../models/dto/login.dto';
 import {RegisterDTO} from '../models/dto/register.dto';
 import {ResetPasswordDTO} from '../models/dto/reset-password.dto';
+import User from '../models/user.model';
 
 export default class AccountService {
   public static authenticate = (data: LoginDTO): Promise<any> => {
@@ -16,16 +17,16 @@ export default class AccountService {
     return axios.get('auth/account/activate/' + code);
   };
 
-  public static requestActivationCode = (user: string): Promise<any> => {
+  public static requestActivationCode = (user: string): Promise<User> => {
     return axios.get('auth/account/request-activation-code/' + user);
-  };
-
-  public static requestResetPasswordCode = (user: string): Promise<Account> => {
-    return axios.get('auth/account/request-reset-password-code/' + user);
   };
 
   public static resetPassword = (data: ResetPasswordDTO): Promise<any> => {
     return axios.post('auth/account/reset-password', data);
+  };
+
+  public static requestResetPasswordCode = (user: string): Promise<User> => {
+    return axios.get('auth/account/request-reset-password-code/' + user);
   };
 
 }
