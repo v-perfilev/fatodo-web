10c1819e0b26f6598caa34ce4d249c723589ec9d
Vladimir Perfilev <persoff68@gmail.com>
2020-06-15 22:31:35 +0200

    TDM-56 change webpack pro to default

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -56,8 +56,8 @@ module.exports = () => merge(commonConfig({env: ENV}), {
     // new BundleAnalyzerPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        BASE_URL: JSON.stringify('http://192.168.178.21:9000'),
-        API_URL: JSON.stringify('https://cfcd21d35f4b.ngrok.io/api'),
+        BASE_URL: JSON.stringify('http://localhost:9000'),
+        API_URL: JSON.stringify('http://localhost:4000/api'),
         RECAPTCHA_KEY: JSON.stringify('6Le5TKQZAAAAANv8EpvGFrvrgoP7HFVMPnPzu-55'),
         SOCIAL_LOGIN: true,
         DEVELOPMENT_MODE: false
