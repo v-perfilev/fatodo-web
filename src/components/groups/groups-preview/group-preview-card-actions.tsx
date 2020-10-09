a9f551983a0b8f15a9393190bb13cebab8f2a3c3
Vladimir Perfilev <persoff68@gmail.com>
2020-10-09 23:33:22 +0200

    TDM-68 add translations to context menu

--- src/components/groups/groups-preview/group-preview-card-actions.tsx
+++ src/components/groups/groups-preview/group-preview-card-actions.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
-import {FC, useState} from 'react';
-import {IconButton, MenuItem} from '@material-ui/core';
-import {DotsVerticalIcon} from '../../common/icons/dots-vertical-icon';
-import {groupCardActionsStyles} from './_styles';
-import {PopupMenu} from '../../common/surfaces/popup-menu';
-import {useHistory} from 'react-router-dom';
-import {GroupRouteUtils} from '../_router';
-import {ItemRouteUtils} from '../../item/_router';
-import {useGroupListContext} from '../../../shared/contexts/group-list-context';
-import {useGroupDeleteContext} from '../../../shared/contexts/group-delete-context';
-import {useGroupViewContext} from '../../../shared/contexts/group-view-context';
+import { FC, useState } from 'react';
+import { IconButton, MenuItem } from '@material-ui/core';
+import { DotsVerticalIcon } from '../../common/icons/dots-vertical-icon';
+import { groupCardActionsStyles } from './_styles';
+import { PopupMenu } from '../../common/surfaces/popup-menu';
+import { useHistory } from 'react-router-dom';
+import { GroupRouteUtils } from '../_router';
+import { ItemRouteUtils } from '../../item/_router';
+import { useGroupListContext } from '../../../shared/contexts/group-list-context';
+import { useGroupDeleteContext } from '../../../shared/contexts/group-delete-context';
+import { useGroupViewContext } from '../../../shared/contexts/group-view-context';
+import { useTranslation } from 'react-i18next';
 
 const GroupPreviewCardActions: FC = () => {
   const classes = groupCardActionsStyles();
   const history = useHistory();
-  const {loadGroups} = useGroupListContext();
-  const {group} = useGroupViewContext();
-  const {setGroupToDelete, setOnDeleteGroupSuccess} = useGroupDeleteContext();
+  const { t } = useTranslation();
+  const { loadGroups } = useGroupListContext();
+  const { group } = useGroupViewContext();
+  const { setGroupToDelete, setOnDeleteGroupSuccess } = useGroupDeleteContext();
   const [anchorEl, setAnchorEl] = useState<HTMLElement>(null);
 
   const isOpen = Boolean(anchorEl);
@@ -60,10 +62,10 @@ const GroupPreviewCardActions: FC = () => {
         <DotsVerticalIcon />
       </IconButton>
       <PopupMenu anchorEl={anchorEl} isOpen={isOpen} onClose={handleClose}>
-        <MenuItem onClick={redirectToItemCreate}>Create item</MenuItem>
-        <MenuItem onClick={redirectToGroupView}>View</MenuItem>
-        <MenuItem onClick={redirectToGroupEdit}>Edit</MenuItem>
-        <MenuItem onClick={openDeleteDialog}>Delete</MenuItem>
+        <MenuItem onClick={redirectToItemCreate}>{t('groups:menu.createItem')}</MenuItem>
+        <MenuItem onClick={redirectToGroupView}>{t('groups:menu.viewGroup')}</MenuItem>
+        <MenuItem onClick={redirectToGroupEdit}>{t('groups:menu.editGroup')}</MenuItem>
+        <MenuItem onClick={openDeleteDialog}>{t('groups:menu.deleteGroup')}</MenuItem>
       </PopupMenu>
     </>
   );
