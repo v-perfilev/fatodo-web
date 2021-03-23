b006f9bcf665c67809fbc38f2b123880eeccd510
Vladimir Perfilev <persoff68@gmail.com>
2021-03-23 09:59:39 +0100

    TDM-75 move ws message destinations to hoc

--- src/shared/hocs/with-ws-client/types.d.ts
+++
@@ -1,8 +0,0 @@
-export enum WsMessageDestinations {
-  CHAT_NEW = '/chat/new',
-  CHAT_UPDATE = '/chat/update',
-  CHAT_DELETE = '/chat/delete',
-  CHAT_LAST_MESSAGE = '/chat/last-message',
-  MESSAGE_NEW = '/message/new',
-  MESSAGE_UPDATE = '/message/update',
-}
--- src/shared/hocs/with-ws-client/with-ws-message-client.tsx
+++ src/shared/hocs/with-ws-client/with-ws-message-client.tsx
@@ -4,8 +4,25 @@ import {WsMessagesContext} from '../../contexts/ws-contexts/ws-messages-context'
 import WsClient from '../../../components/common/ws/ws-client';
 import {Chat} from '../../../models/chat.model';
 import {Message} from '../../../models/message.model';
-import {WsMessageDestinations} from './types';
 import {MESSAGE_WS_URL} from '../../../constants';
+
+enum WsMessageDestinations {
+  CHAT_NEW = '/user/chat/new',
+  CHAT_UPDATE = '/user/chat/update',
+  CHAT_DELETE = '/user/chat/delete',
+  CHAT_LAST_MESSAGE = '/user/chat/last-message',
+  MESSAGE_NEW = '/user/message/new',
+  MESSAGE_UPDATE = '/user/message/update',
+}
+
+const wsMessageTopics = [
+  WsMessageDestinations.CHAT_NEW,
+  WsMessageDestinations.CHAT_UPDATE,
+  WsMessageDestinations.CHAT_DELETE,
+  WsMessageDestinations.CHAT_LAST_MESSAGE,
+  WsMessageDestinations.MESSAGE_NEW,
+  WsMessageDestinations.MESSAGE_UPDATE
+];
 
 const withWsMessageClient = (Component: ComponentType): FC => (props): ReactElement => {
   const [chatNewEvent, setChatNewEvent] = useState<Chat>(null);
@@ -14,15 +31,6 @@ const withWsMessageClient = (Component: ComponentType): FC => (props): ReactElem
   const [chatLastMessageEvent, setChatLastMessageEvent] = useState<Message>(null);
   const [messageNewEvent, setMessageNewEvent] = useState<Message>(null);
   const [messageUpdateEvent, setMessageUpdateEvent] = useState<Message>(null);
-
-  const topics = [
-    WsMessageDestinations.CHAT_NEW,
-    WsMessageDestinations.CHAT_UPDATE,
-    WsMessageDestinations.CHAT_DELETE,
-    WsMessageDestinations.CHAT_LAST_MESSAGE,
-    WsMessageDestinations.MESSAGE_NEW,
-    WsMessageDestinations.MESSAGE_UPDATE
-  ];
 
   const onMessage = (msg: any, topic: string): void => {
     if (topic === WsMessageDestinations.CHAT_NEW) {
@@ -52,7 +60,7 @@ const withWsMessageClient = (Component: ComponentType): FC => (props): ReactElem
   return (
     <WsMessagesContext.Provider value={context}>
       <Component {...props} />
-      <WsClient url={MESSAGE_WS_URL} topics={topics} onMessage={onMessage} />
+      <WsClient url={MESSAGE_WS_URL} topics={wsMessageTopics} onMessage={onMessage} />
     </WsMessagesContext.Provider>
   );
 };
