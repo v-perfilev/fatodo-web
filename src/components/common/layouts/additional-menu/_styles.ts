2fca02eaaf94f1db47f4afa08e6f96061b5b3590
Vladimir Perfilev <persoff68@gmail.com>
2021-06-17 10:15:10 +0300

    TDM-93 edit styles

--- src/components/common/layouts/additional-menu/_styles.ts
+++ src/components/common/layouts/additional-menu/_styles.ts
@@ -2,7 +2,7 @@ import {makeStyles, Theme} from '@material-ui/core/styles';
 
 export const additionalMenuStyles = makeStyles((theme: Theme) => ({
   root: {
-    position: 'absolute',
+    position: 'fixed',
     bottom: theme.spacing(2),
     left: theme.spacing(2),
   },
