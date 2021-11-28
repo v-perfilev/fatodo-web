b8ee71ea1e59627320da345a506588aea96ddf0d
Vladimir Perfilev <persoff68@gmail.com>
2021-11-28 11:15:59 +0100

    TDM-112 remove extra line after description in item view

--- src/pages/item/item-view/item-view-description.tsx
+++ src/pages/item/item-view/item-view-description.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
 import {Box} from '@material-ui/core';
 import {useTranslation} from 'react-i18next';
-import {LabeledBox, PageDivider} from '../../../components/surfaces';
+import {LabeledBox} from '../../../components/surfaces';
 import {useItemViewContext} from '../../../shared/contexts/view-contexts/item-view-context';
 import {itemViewCommonStyles} from './_styles';
 import ItemViewDescriptionStub from './item-view-description-stub';
@@ -12,12 +12,9 @@ const ItemViewDescription: FC = () => {
   const {obj: item} = useItemViewContext();
 
   return item.description ? (
-    <>
-      <LabeledBox label={t('item:labels.description')} className={commonClasses.box}>
-        <Box>{item.description}</Box>
-      </LabeledBox>
-      <PageDivider />
-    </>
+    <LabeledBox className={commonClasses.box} label={t('item:labels.description')}>
+      <Box>{item.description}</Box>
+    </LabeledBox>
   ) : (
     <ItemViewDescriptionStub />
   );
