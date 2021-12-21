45c2edca4cbdf28d8a92335efe0fa81c740b5162
Vladimir Perfilev <persoff68@gmail.com>
2021-12-21 09:27:10 +0300

    TDM-123 changed avatars count in shorten form

--- src/pages/group/_constants.ts
+++ src/pages/group/_constants.ts
@@ -11,5 +11,5 @@ export const GROUP_ITEM_HEIGHT = 60;
 export const GROUP_ITEMS_COUNT = 10;
 
 export const AVATARS_IN_CARD = 7;
-export const SHORTEN_AVATARS_IN_CARD = 1;
+export const SHORTEN_AVATARS_IN_CARD = 3;
 export const PAGINATION_BUTTON_WIDTH = 32;
